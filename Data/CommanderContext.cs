5fe78af44b2791101d41684970e476678b5ea2a4
Shubhangi D <test@gssddma.com>
2021-05-17 18:19:43 +0200

    WebApi first commit

---
+++ Data/CommanderContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt): base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,43 @@
+﻿using Commander.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public void CreateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command>
+            {
+                 new Command { Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Kettle and Pan" },
+                 new Command { Id = 1, HowTo = "Cut Bread", Line = "Get a knife", Platform = "Knife and Chopping board" },
+                 new Command { Id = 2, HowTo = "Make a cup of Tea", Line = "Place teabag in cup", Platform = "Kettle and cup" }
+            };
+            return commands;
+    }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an Egg", Line = "Boil Water", Platform = "Kettle and Pan" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCommand(Command cmd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
